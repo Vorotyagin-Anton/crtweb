2983a231437b5bb5ec01affed2ec8a6bf90d169d
VorotyaginAnton <a.vorotyagin@yandex.ru>
2022-10-02 23:20:35 +0300

    refactor(HomeController): use home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,44 +2,27 @@
 
 namespace App\Controller;
 
-use App\Entity\Movie;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\EntityManagerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Slim\Exception\HttpBadRequestException;
-use Slim\Interfaces\RouteCollectorInterface;
+use Slim\Exception\HttpInternalServerErrorException;
 use Twig\Environment;
 
 class HomeController
 {
-    public function __construct(
-        private RouteCollectorInterface $routeCollector,
-        private Environment $twig,
-        private EntityManagerInterface $em
-    ) {}
+    public function __construct(private Environment $twig)
+    {
+    }
 
     public function index(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         try {
-            $data = $this->twig->render('home/index.html.twig', [
-                'trailers' => $this->fetchData(),
-            ]);
+            $data = $this->twig->render('home/index.html.twig');
         } catch (\Exception $e) {
-            throw new HttpBadRequestException($request, $e->getMessage(), $e);
+            throw new HttpInternalServerErrorException($request, $e->getMessage(), $e);
         }
 
         $response->getBody()->write($data);
 
         return $response;
-    }
-
-    protected function fetchData(): Collection
-    {
-        $data = $this->em->getRepository(Movie::class)
-            ->findAll();
-
-        return new ArrayCollection($data);
     }
 }
